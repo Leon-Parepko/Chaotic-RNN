93766279538273ad2316a19d25b629b0d9200503
Leon-Parepko <l.parepko@innopolis.university>
2023-04-24 20:31:17 +0300

    Weight initialization now  working properly.

--- Architecture/Layers.py
+++ Architecture/Layers.py
@@ -1,4 +1,3 @@
-import math
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -18,12 +17,12 @@ class Single_Neuron_Node:
         pass
 
 class Random_RNN(nn.Module):
-
     __constants__ = ['in_features', 'out_features']
     in_features: int
     out_features: int
     input_weights: Tensor
     associative_weights: Tensor
+
 
     def get_connections_num(self):
         """
@@ -53,20 +52,17 @@ class Random_RNN(nn.Module):
          them as an attributes of edges.
         :return: None
         """
-
         input_w_counter = 0
         ass_w_counter = 0
 
         for node in self.graph.nodes(data=True):
             for edge in self.graph.edges(node[0], data=True):
                 if node[1]["type"] == 'input':
-                    # TODO: FIX ASSIGNMENT
-                    edge["weight"] = self.input_weights[input_w_counter]
+                    edge[2]["weight"] = self.input_weights[input_w_counter]
                     input_w_counter += 1
 
                 elif node[1]["type"] == 'associative':
-                    # TODO: FIX ASSIGNMENT
-                    edge["weight"] = self.associative_weights[ass_w_counter]
+                    edge[2]["weight"] = self.associative_weights[ass_w_counter]
                     ass_w_counter += 1
 
 
@@ -222,155 +218,3 @@ class Random_RNN(nn.Module):
         # forward all input neurons
         for i in range(0, self.in_features):
             pass
-
-
-
-
-
-
-
-    def history_update(self, post_spikes=None):
-        if post_spikes is not None:
-            for i, post_spike in enumerate(post_spikes):
-                # If post spike, reset time for post spike in history
-                if post_spike > 0:
-                    self.spike_history["post"][i] = 1
-
-                # Else increase time for post spike in history
-                elif self.spike_history["post"][i] >= 1:
-                    self.spike_history["post"][i] += 1
-        return self.spike_history["cur"]
-
-
-
-    def get_membrane_potential(self):
-        return self.v
-
-    def get_spike_history(self):
-        return self.spike_history
-
-    def weight_reset(self):
-        with torch.no_grad():
-            self.weight.zero_()
-
-    def weight_to_one(self):
-        with torch.no_grad():
-            for i in range(0, self.weight.shape[0]):
-                for j in range(0, self.weight.shape[1]):
-                    self.weight[i][j] = 1
-
-
-class TemporalEncoder(nn.Module):
-    def __init__(self, in_features, out_features, tau=1, device=None, dtype=None):
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(TemporalEncoder, self).__init__()
-        self.weight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
-        self.weight.data.uniform_(0, 1)
-        self.in_features = in_features
-        self.out_features = out_features
-        self.tau = tau
-        self.spike_history = SpikeHistory(post=torch.zeros(out_features, **factory_kwargs))
-
-    def forward(self, x, t):
-        # Initialize the output
-        spikes = torch.zeros(self.in_features)
-
-        # Normalize input and set it from 0 to 255
-        x = (x / torch.max(x)) * 255
-
-        # Discretize input using tau
-        x = torch.floor(x / self.tau)
-
-        for i in range(x.shape[1]):
-            if torch.tensor(t, dtype=torch.int32) == torch.tensor(x, dtype=torch.int32)[0][i]:
-                spikes[i] = 1
-
-        # Update spike history
-        self.spike_history["cur"] = spikes
-        return self.weight * spikes
-
-
-    def history_update(self, post_spikes):
-        for i, post_spike in enumerate(post_spikes):
-            # If post spike, reset time for post spike in history
-            if post_spike > 0:
-                self.spike_history["post"][i] = 1
-
-            # Else increase time for post spike in history
-            elif self.spike_history["post"][i] >= 1:
-                self.spike_history["post"][i] += 1
-
-
-    def neuron_states(self):
-        return self.spikes
-
-    def get_spike_history(self):
-        return self.spike_history
-
-    def weight_reset(self):
-        with torch.no_grad():
-            self.weight.zero_()
-
-    def weight_to_one(self):
-        with torch.no_grad():
-            for i in range(0, self.weight.shape[0]):
-                for j in range(0, self.weight.shape[1]):
-                    self.weight[i][j] = 1
-
-
-class RateEncoder(nn.Module):
-    def __init__(self, in_features, out_features, tau=1, device=None, dtype=None):
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(RateEncoder, self).__init__()
-        self.weight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
-        self.weight.data.uniform_(0, 1)
-        self.in_features = in_features
-        self.out_features = out_features
-        self.tau = tau
-        self.spike_history = SpikeHistory(post=torch.zeros(out_features, **factory_kwargs))
-
-    def forward(self, x, t):
-        # Initialize the output
-        spikes = torch.zeros(self.in_features)
-
-        # Normalize input and set it from 0 to 255
-        x = (x / torch.max(x)) * 255
-
-        # Discretize input using tau
-        x = torch.floor(x / self.tau)
-
-        for i in range(x.shape[1]):
-            if torch.tensor(t, dtype=torch.int32) == torch.tensor(x, dtype=torch.int32)[0][i]:
-                spikes[i] = 1
-
-        # Update spike history
-        self.spike_history["cur"] = spikes
-        return self.weight * spikes
-
-
-    def history_update(self, post_spikes):
-        for i, post_spike in enumerate(post_spikes):
-            # If post spike, reset time for post spike in history
-            if post_spike > 0:
-                self.spike_history["post"][i] = 1
-
-            # Else increase time for post spike in history
-            elif self.spike_history["post"][i] >= 1:
-                self.spike_history["post"][i] += 1
-
-
-    def neuron_states(self):
-        return self.spikes
-
-    def get_spike_history(self):
-        return self.spike_history
-
-    def weight_reset(self):
-        with torch.no_grad():
-            self.weight.zero_()
-
-    def weight_to_one(self):
-        with torch.no_grad():
-            for i in range(0, self.weight.shape[0]):
-                for j in range(0, self.weight.shape[1]):
-                    self.weight[i][j] = 1
